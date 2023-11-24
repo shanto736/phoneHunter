5af410330f1175a17e4acc2635191c8f6610c932
shanto736 <bokhtear.uddin96@gmail.com>
2023-11-24 20:53:25 +0600

    2nd day session

--- phone.js
+++ phone.js
@@ -1,6 +1,26 @@
 const loadphone = async () =>{
     const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
     const data = await res.json();
-    console.log(data.data);
+    const phones = data.data;
+    displayPhones(phones);
+}
+const phoneContainer = document.getElementById('phone-container');
+const displayPhones = phones =>{
+    phones.forEach(phone => {
+        console.log(phone);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList= `card bg-base-100 bg-sky-200 p-4 shadow-xl`;
+        phoneCard.innerHTML=`
+        <figure><img src="${phone.image}" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+        ` 
+        phoneContainer.appendChild(phoneCard);
+    });
 }
 loadphone();
