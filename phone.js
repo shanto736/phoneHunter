e45aab1cd4afd1576a57a60765b49a4fd574e54c
shanto736 <bokhtear.uddin96@gmail.com>
2023-11-24 19:43:02 +0600

    2nd day

--- phone.js
+++ phone.js
@@ -1,1 +1,6 @@
-console.log("hunting");
+const loadphone = async () =>{
+    const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
+    const data = await res.json();
+    console.log(data.data);
+}
+loadphone();
